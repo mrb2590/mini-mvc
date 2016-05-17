45c23672d3d70a97eef8ce2abe63beed4d4a2724
mrb2590 <mrb2590@gmail.com>
2016-05-16 22:44:58 -0400

    Added a mysqli class

---
+++ MiniMVC/Model/SQLTable.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * SQLTable.php
+ * 
+ * MiniMVC
+ * 
+ * @author Mike Buonomo <mrb2590@gmail.com>
+ */
+
+namespace MiniMVC\Model;
+
+class SQLTable
+{
+    /**
+     * @var mysqli $mysqli  The mysqli object
+     */
+    protected $mysqli;
+
+    /**
+     * Instantiate the mysqli class. This requires the database credentials from mysqli.php
+     */
+    public function __construct()
+    {
+        $dbCreds = require DOC_ROOT . '/mysqli.php';
+        $this->mysqli = new mysqli(
+            $dbCreds['host'],
+            $dbCreds['user'],
+            $dbCreds['pass'],
+            $dbCreds['db']
+        );
+        if ($this->mysqli->connect_errno > 0) {
+            throw new Exception('Unable to connect to database [' . $this->mysqli->connect_error . ']');
+        }
+    }
+}
---
+++ mysqli.example.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+    "host" => "localhost",
+    "user" => "mysqluser",
+    "pass" => "mysqlpassword",
+    "db"   => "mini-mvc"
+);
